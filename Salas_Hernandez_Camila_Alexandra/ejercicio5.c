5f7be4e8c980e3240a183df1182d6f0efafab8db
CamilaAlexandraSalas <112556680+CamilaAlexandraSalas@users.noreply.github.com>
2025-04-28 00:40:18 -0600

    Subiendo ejercicio5 Final

--- Salas_Hernandez_Camila_Alexandra/ejercicio5.c
+++ Salas_Hernandez_Camila_Alexandra/ejercicio5.c
@@ -14,22 +14,41 @@ int main(){
     //Creamos un arreglo dinamico de 5 libros por medio de malloc
     struct Libro* Libros = (struct Libro*)malloc(5 * sizeof(struct Libro));
 
-    //Llenamos los valores de los 5 libros que hicimos anteriormente 
-    struct Libro libros[5] = {
-        {"A todos los chicos de los que me enamore", "Jenny Han", "Destino", 2014},
-        {"Despues de ti", "Jojo Moyes", "Suma", 2015},
-        {"Heartstopper", "Alice Oseman", "VRYA", 2018},
-        {"Boulevard", "Flor M. Salvador", "Naranja", 2019},
-        {"Orgullo y prejuicio", "Jane Austen", "T. Egerton", 1813}
-    };
+    //Llenamos los titulos de los 5 libros
+    strcpy(Libros[0].titulo, "A todos los chicos de los que me enamore");
+    strcpy(Libros[1].titulo, "Despues de ti");
+    strcpy(Libros[2].titulo, "Heartstopper");
+    strcpy(Libros[3].titulo, "Boulevard");
+    strcpy(Libros[4].titulo, "Orgullo y prejuicio");
+
+    //Llenamos los autores de los 5 libros
+    strcpy(Libros[0].autor, "Jenny Han");
+    strcpy(Libros[1].autor, "Jojo Moyes");
+    strcpy(Libros[2].autor, "Alice Oseman");
+    strcpy(Libros[3].autor, "Flor Salvador");
+    strcpy(Libros[4].autor, "Jane Austen");
+
+    //Llenamos las editoriales de los 5 libros
+    strcpy(Libros[0].editorial, "Destino");
+    strcpy(Libros[1].editorial, "Suma");
+    strcpy(Libros[2].editorial, "VRYA");
+    strcpy(Libros[3].editorial, "Naranja");
+    strcpy(Libros[4].editorial, "T. Egerton");
+
+    //Llenando el año de los 5 libros
+    Libros[0].anio = 2014;
+    Libros[1].anio = 2015;
+    Libros[2].anio = 2018;
+    Libros[3].anio = 2019;
+    Libros[4].anio = 1813;
 
     //Imprimimos los valores de cada libro
     printf("-----Libros-----\n");
     for (int i = 0; i < 5; i++) {
-        printf("*Libro: %s\n", libros[i].titulo);
-        printf("\tAutor: %s\n", libros[i].autor);
-        printf("\tEditorial: %s\n", libros[i].editorial);
-        printf("\tAnio: %d\n\n", libros[i].anio);
+        printf("*Libro: %s\n", Libros[i].titulo);
+        printf("\tAutor: %s\n", Libros[i].autor);
+        printf("\tEditorial: %s\n", Libros[i].editorial);
+        printf("\tAnio: %d\n\n", Libros[i].anio);
 
     }
 
