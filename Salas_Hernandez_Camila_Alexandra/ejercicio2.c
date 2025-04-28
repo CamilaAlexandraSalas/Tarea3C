b697be56b9bf5ccd402eb3332088a25aed3cc3c8
CamilaAlexandraSalas <112556680+CamilaAlexandraSalas@users.noreply.github.com>
2025-04-28 00:09:00 -0600

    Subiendo ejercicio2 Final

--- Salas_Hernandez_Camila_Alexandra/ejercicio2.c
+++ Salas_Hernandez_Camila_Alexandra/ejercicio2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-//Se define un struct llamado Producto, contiene 3 miembros
+//Se define un struct llamado Producto, contiene 3 miembros: nombre, precio, cantidad
 struct Producto {
     char nombre[50];
     int precio;
@@ -9,37 +9,29 @@ struct Producto {
 };
 
 int main(){
-    int valorTotalInventario = 0;
-    //Se declara un arreglo de 4 productos
-    struct Producto productos[4];
-
-    //Llenado el primer producto
-    strcpy(productos[0].nombre, "Yogurt");
-    productos[0].precio = 30;
-    productos[0].cantidad = 10;
-
-    //Llenado el segundo producto
-    strcpy(productos[1].nombre, "Crema");
-    productos[1].precio = 50;
-    productos[1].cantidad = 8;
-
-    //Llenando el tercer producto
-    strcpy(productos[2].nombre, "Lechera");
-    productos[2].precio = 60;
-    productos[2].cantidad = 6;
-
-    //Llenando el cuarto producto
-    strcpy(productos[3].nombre, "Gelatina");
-    productos[3].precio = 25;
-    productos[3].cantidad = 12;
+    //Inicializamos la variable en 0 para despues poder calcular el inventario 
+    int valorTotalInventario=0;
+    //Se declara un arreglo de 10 productos, y los llenamos con sus valores
+    struct Producto productos[10]={
+        {"Yogurt", 30, 10},
+        {"Crema", 50, 8},
+        {"Lechera", 60, 6},
+        {"Gelatina", 25, 12},
+        {"Danonino", 10, 4},
+        {"Yakult", 8, 10},
+        {"Leche", 30, 12},
+        {"Flan", 15, 5},
+        {"Tostadas", 25, 6},
+        {"Pan", 10, 3}
+    };
     
     //Calculando el valor total del inventario
-    for (int i = 0; i < 4; i++) {
-        valorTotalInventario += productos[i].precio * productos[i].cantidad;
+    for (int i=0; i<10; i++) {
+        valorTotalInventario+=productos[i].precio*productos[i].cantidad;
     }
 
-    //Imprimiendo el valor total del inventario
-    printf("El valor total del inventario es: %d\n", valorTotalInventario);
+    //Imprimiendo el valor total de inventario
+    printf("Valor total de inventario:$%d\n", valorTotalInventario);
 
 }
 
