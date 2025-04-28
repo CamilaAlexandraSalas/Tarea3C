1c923777b7975900a0a307a4ada1abbef70a79e9
vapemo <vale_perez@ciencias.unam.mx>
2025-04-28 04:41:06 -0600

    ejercicio1

---
+++ Pérez_Mondragón_Valeria/ejercicio1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+// Definición de la estructura Coche
+struct Coche {
+    char marca[50];
+    char modelo[50];
+    int año;
+    float precio;
+};
+
+int main() {
+    // instanciade Coche
+    struct Coche miCoche;
+    
+    // asignamos valores  a Coche
+    strcpy(miCoche.marca, "Toyota");
+    strcpy(miCoche.modelo, "Corolla");
+    miCoche.año = 2020;
+    miCoche.precio = 25000.50;
+    
+    // salida de los valores
+    printf("Datos originales del coche:\n");
+    printf("Marca: %s\n", miCoche.marca);
+    printf("Modelo: %s\n", miCoche.modelo);
+    printf("Año: %d\n", miCoche.año);
+    printf("Precio: %.2f\n\n", miCoche.precio);
+    
+    // el apuntador a  Coche
+    struct Coche *ptrCoche;
+    
+    // asignamos la dirección de miCoche al apuntador
+    ptrCoche = &miCoche;
+    
+    // modificación de los vaslores usando el apuntador
+    strcpy(ptrCoche->marca, "Honda");  
+    strcpy(ptrCoche->modelo, "Civic");
+    ptrCoche->año = 2022;
+    ptrCoche->precio = 27500.75;
+    
+    // salida de los valores modificados
+    printf("Datos modificados del coche:\n");
+    printf("Marca: %s\n", ptrCoche->marca);
+    printf("Modelo: %s\n", ptrCoche->modelo);
+    printf("Año: %d\n", ptrCoche->año);
+    printf("Precio: %.2f\n", ptrCoche->precio);
+    
+    return 0;
+}
