1d85e6b3c50a7a2c2e1aa491507c49287b380d1d
CamilaAlexandraSalas <112556680+CamilaAlexandraSalas@users.noreply.github.com>
2025-04-27 20:29:43 -0600

    Subiendo ejercicio3

---
+++ Salas_Hernandez_Camila_Alexandra/ejercicio3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>  
+#include <time.h>
+
+//Se define un struct llamado Punto, contiene 2 miembros
+struct Punto {
+    int x;
+    int y;
+};
+
+//Definimos la funcion NumAleatorio que retorna una estructura Punto
+struct Punto NumAleatorio(){
+    //Se define una variable punto de tipo struct Punto
+    struct Punto punto;
+
+    //Inicializar el generador de numero aleatorios
+    srand(time(NULL));
+    //Generamos los numeros aleatorios para las dos variables
+    punto.x = rand()%100;
+    punto.y = rand()%100;
+
+    //Se retorna la variable punto
+    return punto;
+}
+
+int main(){
+
+    //Creamos una variable punto de tipo struct Punto que va a recibir lo que retorna la funcion NumAleatorio
+    struct Punto punto = NumAleatorio();
+    //Se imprimen los valores
+    printf("Coordenada x: %d\n", punto.x);
+    printf("Coordenada y: %d\n", punto.y);
+
+}
